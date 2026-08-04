362032acf9780121e5d0dd549b0e15a7fbabd6b2
Anand Raj <anandraj7008@gmail.com>
2026-08-04 17:47:58 +0530

    push to main

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -9,6 +9,8 @@ import LoaderOverLay from "./components/loader/RouterLoader";
 import AnalyticsTracker from "./components/AnalyticsTracker";
 import WhatsAppButton from "./components/WhatsAppButton";
 
+import InternshipModal from "./components/modal/InternshipModal";
+
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
@@ -20,14 +22,21 @@ function App() {
   const location = useLocation();
 
   const [loading, setLoading] = useState(false);
+  const [openInternship, setOpenInternship] = useState(false);
 
-  const modalRef = useRef();
+  // const modalRef = useRef();
 
   const { sendEmails } = useEmailSender();
 
   function handleInternshipModal() {
-    modalRef.current?.open();
+    setOpenInternship(true);
   }
+
+  useEffect(() => {
+    if (location.pathname === "/" && location.hash === "#internship") {
+      setOpenInternship(true);
+    }
+  }, [location]);
 
   async function onSubmitFormRequest(formData, INFOTYPE) {
     return await sendEmails(formData, INFOTYPE);
@@ -61,7 +70,7 @@ function App() {
             element={
               route.path === "/" ? (
                 <route.element.type
-                  modalRef={modalRef}
+                  
                   onSubmitFormRequest={onSubmitFormRequest}
                 />
               ) : (
@@ -71,6 +80,13 @@ function App() {
           />
         ))}
       </Routes>
+
+      {openInternship && (
+  <InternshipModal
+    onClose={() => setOpenInternship(false)}
+    onSubmitFormRequest={onSubmitFormRequest}
+  />
+)}
 
       <Footer
         onSubmitFormRequest={onSubmitFormRequest}
